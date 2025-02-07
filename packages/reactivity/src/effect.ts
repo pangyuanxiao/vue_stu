ef859b57f961f3c1c315a9feaf171e52881a52d5
pangyuanxiao <1079044182@qq.com>
2025-02-07 15:55:47 +0800

    5-8 effect的框架

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -5,3 +5,28 @@ export function track(target: object, key: unknown) {
 export function trigger(target: object, key?: unknown) {
   console.log(`触发依赖`)
 }
+
+/**
+ * effect 函数
+ * @param fn 执行方法
+ * @returns 以 ReactiveEffect 实例为 this 的执行函数
+ */
+export function effect<T = any>(fn: () => T) {
+  // 生成 ReactiveEffect 实例
+  const _effect = new ReactiveEffect(fn)
+  _effect.run()
+}
+
+/**
+ * 单例的，当前的 effect
+ */
+export let activeEffect: ReactiveEffect | undefined
+
+export class ReactiveEffect<T = any> {
+  constructor(public fn: () => T) {}
+
+  run() {
+    activeEffect = this
+    return this.fn()
+  }
+}
